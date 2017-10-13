542f97d3e591815f14411c9c426bf949b67a62fb
Olivier Robardet <orobardet.ext@orange.com>
2017-10-13 17:38:38 +0200

    cli: add author and version display factoring

--- main.go
+++ main.go
@@ -11,27 +11,29 @@ var VERSION = "0.0.0-dev"
 var REVISION = "HEAD"
 var BUILD_TIME = ""
 
-func printVersion(c *cli.Context) {
-	fmt.Printf("version=%s revision=%s built on=%s\n", VERSION, REVISION, BUILD_TIME)
-}
-
 func main() {
-	cli.VersionPrinter = printVersion
+	cli.VersionPrinter = func(c *cli.Context) {
+		fmt.Printf("version=%s revision=%s built on=%s\n", VERSION, REVISION, BUILD_TIME)
+	}
 
 	app := cli.NewApp()
 	app.Name = "gitlab-ci-linter"
 	app.Version = fmt.Sprintf("%s (%s)", VERSION, REVISION[:int(math.Min(float64(len(REVISION)), 7))])
+	app.Authors = []cli.Author{
+		{Name: "Olivier ROBARDET"},
+	}
 	app.Usage = "lint your .gitlab-ci.yml using the Gitlab lint API"
 	app.EnableBashCompletion = true
 	app.Commands = []cli.Command{
 		{
-			Name: "version",
+			Name:    "version",
 			Aliases: []string{"v"},
-			Usage: "Show version information",
-			Action: printVersion,
+			Usage:   "Show version information",
+			Action: func(c *cli.Context) {
+				cli.ShowVersion(c)
+			},
 		},
 	}
-
 	app.Action = func(c *cli.Context) error {
 		fmt.Printf("Lint the ci")
 		return nil
